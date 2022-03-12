c86e2d3c49030d55be1a5d7765b3f9960ec613e6
jaehyuneyea <jaehyune.yea@gmail.com>
2022-03-11 22:09:32 -0800

    Completed rock button functionality

--- script.js
+++ script.js
@@ -22,39 +22,51 @@ function computerPlay() {
 function playRound(playerSelection, computerSelection) {
   let playerCase = playerSelection.toLowerCase();
   console.log(playerCase);
+  console.log(computerSelection);
   let winState;
 
   if (playerCase == computerSelection) { // tie case is when both selections are just equal
     winState = `It's a tie! ${playerCase} ties with ${computerSelection}`; // we store the game result string in a variable
-    return winState;
+    updateVictory(winState);
   } else {
     switch (playerCase) { // check the cases in a switch statement for all possible values
       case "rock":
         (computerSelection == "scissors") ? winState = `You win! ${playerCase} beats ${computerSelection}` : 
         winState = `You lose! ${computerSelection} beats ${playerCase}`; // win if scissors, lose if paper
-        return winState;
+        updateVictory(winState);
+        break;
       case "paper":
         (computerSelection == "rock") ? winState = `You win! ${playerCase} beats ${computerSelection}` : 
         winState = `You lose! ${computerSelection} beats ${playerCase}`;
-        return winState;
+        updateVictory(winState);
+        break;
       case "scissors":
         (computerSelection == "paper") ? winState = `You win! ${playerCase} beats ${computerSelection}` : 
         winState = `You lose! ${computerSelection} beats ${playerCase}`;
-        return winState;
-      default:
-        winState = "Choose a valid choice!";
-        return winState;
+        updateVictory(winState);
     }
   }
 }
+
+function updateVictory(winState) {
+  // stub
+  let desc = document.querySelector('#description');
+  desc.textContent = winState;
+}
+
 // prompts user for a choice and runs playRound 5 times and accepts only valid input
 // function game() {
-//   for (let i = 0; i < 5; i++) {
 //     let playerSelection = prompt("Choose rock, paper or scissors").toLowerCase();
 //     let computerSelection = computerPlay();
 //     let result = playRound(playerSelection, computerSelection);
-//     (result == "Choose a valid choice!") ? i -= 1 : i; // ensures that 5 valid inputs are made 
 //     console.log(result);
-
-//   }
 // }
+
+const container = document.querySelector('.container')
+
+const rock = document.querySelector('#rock');
+rock.addEventListener('click', () => {
+  let playerSelection = 'rock';
+  let computerSelection = computerPlay();
+  playRound(playerSelection,computerSelection);
+});
